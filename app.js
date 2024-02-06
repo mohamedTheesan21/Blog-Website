7620e0ca4833bc172ff5192ffb9c8e12ce69d7cb
Theesan <mohamedtheesan4@gmail.com>
2024-02-06 07:20:31 +0530

    header file added

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 //jshint es6
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+const homeStartingContent =
+  "This is a simple blog website. You can add your blog by clicking on the compose button. You can also read the blogs by clicking on the read more button. You can also delete the blog by clicking on the delete button. You can also update the blog by clicking on the update";
 
-app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
-app.get('/', function(req, res){
-    res.render("home")
-})
+app.get("/", function (req, res) {
+  res.render("home", { homeStartingContent: homeStartingContent });
+});
 
-app.post('/', function(req, res){
-    res.redirect('/')
-})
+app.post("/", function (req, res) {
+  res.redirect("/");
+});
 
-app.listen(3000, function(){
-    console.log("Server started on port 3000");
-})
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
